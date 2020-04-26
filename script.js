47fc24604826907ae648a703f2a1afa0ded6a3ad
aaamenezes <57503927+aaamenezes@users.noreply.github.com>
2020-04-26 19:11:15 -0300

    Já está funcional
    Agora resta tratar melhor os dados recebidos (ex.: só aceitar 1s e 0s no campo de converter de binário para decimal).
    
    Também melhorar o visual

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-let decimal = document.getElementById("decimal")
-/*
-let binario = document.getElementById("binario")
-*/
+let decimal
 
 function converteEmBinario() {
-    
     // Definição
-    decimal = parseInt(decimal.value)
-    let binario
+    decimal = parseInt(document.getElementById("decimal").value)
     let lista = []
 
     // Divisão por 2
@@ -15,6 +10,13 @@ function converteEmBinario() {
         lista.push(decimal % 2)
         decimal = parseInt(decimal / 2)
     }
+
+    // Completar com 0s até ficar com 8 dígitos
+    while (lista.length < 8) {
+        lista.push(0)
+    }
+
+    lista.reverse()
     
     // Construindo o binario
     binario = lista.join("")
@@ -24,8 +26,36 @@ function converteEmBinario() {
 
 }
 
+let binario
+
 function converteEmDecimal(binario) {
-    alert("talkey")
+    
+    // Definição
+    binario = (document.getElementById("binario").value).split('')
+
+    // Transformar posições da array em number
+    let i
+    for (i = 0; i < binario.length; i++) {
+        if (typeof binario[i] == "string") {
+            binario[i] = parseInt(binario[i])
+        }
+    }
+    
+    // Completar com 0s até ficar com 8 dígitos
+    while (binario.length < 8) {
+        binario.unshift(0)
+    }
+
+    // Construindo o decimal
+    let exponencial = 7
+    decimal = 0
+    for (i = 0; i < binario.length; i++) {
+        decimal += binario[i]*(2**exponencial)
+        exponencial--
+    }
+
+    // Resultado final
+    resultadoEmDecimal.innerHTML = decimal
 }
 
 
