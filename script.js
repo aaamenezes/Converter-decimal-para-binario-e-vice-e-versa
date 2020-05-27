cc6fcded057d4de78f76ba605398a671a1a75cb6
aaamenezes <57503927+aaamenezes@users.noreply.github.com>
2020-05-27 18:35:45 -0300

    Remover comentários no final do arquivo script.js

--- script.js
+++ script.js
@@ -57,36 +57,3 @@ function converteEmDecimal(binario) {
     // Resultado final
     resultadoEmDecimal.innerHTML = decimal
 }
-
-
-/*
-base10 = 100
-
-function changeAds(base10):
-    lista = []
-    while base10 > 0:
-        lista.append(base10%2)
-        base10 = base10 // 2
-    console.log(lista, "Lista")
-    lista.reverse()
-    console.log(lista, "Lista invertida")
-    p = 0
-    while p < len(lista):
-        if lista[p] == 1:
-            lista[p] = 0
-        else:
-            lista[p] = 1
-        p +=1
-    console.log(lista, "Lista reversa ")
-    expo = len(lista) - 1
-    console.log(expo)
-    numeroSaida = 0
-    for i in lista:
-        numeroSaida += i*(2**expo)
-        expo -= 1
-    console.log(numeroSaida, "Resultado ")
-    console.log("-----------------------")
-    return numeroSaida
-
-changeAds(base10)
-*/
